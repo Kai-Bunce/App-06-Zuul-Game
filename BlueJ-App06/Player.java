26d932306a4544bda99a93df3301a71a7bfa2fe9
Kai-Bunce <71985097+Kai-Bunce@users.noreply.github.com>
2020-12-02 20:22:47 +0000

    Player class created

---
+++ BlueJ-App06/Player.java
@@ -0,0 +1,33 @@
+
+/**
+ * The player class is the users character and can be used and played with
+ *
+ * @author (Kai Bunce)
+ * @version Version 1
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
